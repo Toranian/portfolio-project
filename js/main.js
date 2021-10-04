3d06f5744d1ab12c9f7f59ac19e88c67a0e05088
Isaac Morrow <isaacmorrowofficial@gmail.com>
2021-10-03 23:49:20 -0700

    Added nav toggle - needs fixing

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ window.onload = function () {
 		element.classList.remove("fade");
 		element.classList.add("show");
 	}
-
-
 }
 
 function scale(number, inMin, inMax, outMin, outMax) {
@@ -16,7 +14,7 @@ function scale(number, inMin, inMax, outMin, outMax) {
 
 window.onscroll = function () {
 	var scrollbar = document.getElementById("scrollbar");
-	
+
 	var height = document.documentElement.scrollTop;
 	if (height > 0) {
 		scrollbar.style.height = scale(height, 0, window.innerHeight, 0, 100) + "%";
@@ -32,5 +30,19 @@ function fadeOut() {
 		element = elements[i];
 		element.classList.remove("show");
 		element.classList.add("fade");
+	}
+}
+
+function toggleNav() {
+	var navbar = document.querySelector("#navbar");
+	var menu = document.querySelector("#menu");
+
+	menu.classList.toggle("change");
+	navbar.classList.toggle("hide");
+}
+
+window.onresize = function () {
+	if (window.innerWidth <= 1000) {
+		toggleNav();
 	}
 }
